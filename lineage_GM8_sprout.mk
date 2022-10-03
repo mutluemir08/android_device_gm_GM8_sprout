f409990acfd2d1ca451bae979e7673a9bdc97dc1
Mutlu Öztürk <49791081+mutluemir08@users.noreply.github.com>
2022-10-03 13:05:25 +0300

    Update and rename arrow_GM8_sprout.mk to lineage_GM8_sprout.mk

--- lineage_GM8_sprout.mk
+++ lineage_GM8_sprout.mk
@@ -8,10 +8,10 @@
 $(call inherit-product, device/gm/GM8_sprout/device.mk)
 
 # Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_GM8_sprout
+PRODUCT_NAME := lineage_GM8_sprout
 PRODUCT_DEVICE := GM8_sprout
 PRODUCT_BRAND := GM
 PRODUCT_MODEL := GM 8
